09f7be34783725f7ba7061ff2cea6c46e6b5d81e
Bealvs <beatrizalves2083@gmail.com>
2026-03-24 12:20:58 -0300

    fix: update insert method to include isConstant parameter in SymbolTable

--- src/main/java/com/uepb/compiler/SymbolTable.java
+++ src/main/java/com/uepb/compiler/SymbolTable.java
@@ -6,15 +6,15 @@ import java.util.Map;
 public class SymbolTable {
     private final Map<String, Variables> table = new HashMap<>();
 
-    public void insert(String name, int address){
-        table.put(name, new Variables(name, address));
+    public void insert(String name, int address, boolean isConstant) {
+        table.put(name, new Variables(name, address, isConstant));
     }
 
-    public boolean exists(String name){
+    public boolean exists(String name) {
         return table.containsKey(name);
     }
 
-    public Variables get(String name){
+    public Variables get(String name) {
         return table.get(name);
     }
 
